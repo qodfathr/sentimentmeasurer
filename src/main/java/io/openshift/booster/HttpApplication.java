0d38fd8490e63c63b0320f9bcb754f4c7a5ad291
Todd Mancini <todd.mancini@daxat.com>
2018-05-04 13:13:28 +0000

    Code cleanup

--- src/main/java/io/openshift/booster/HttpApplication.java
+++ src/main/java/io/openshift/booster/HttpApplication.java
@@ -21,11 +21,6 @@ import io.vertx.core.json.JsonArray;
 import io.vertx.ext.healthchecks.HealthCheckHandler;
 import io.vertx.ext.healthchecks.Status;
 
-import io.vertx.ext.auth.oauth2.OAuth2Auth;
-import io.vertx.ext.auth.oauth2.providers.TwitterAuth;
-import io.vertx.ext.web.codec.BodyCodec;
-import io.vertx.rxjava.ext.auth.oauth2.AccessToken;
-
 import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
 
 import twitter4j.*;
@@ -38,7 +33,9 @@ import java.util.Set;
 import io.vertx.core.impl.ConcurrentHashSet;
 import io.vertx.core.eventbus.MessageConsumer;
 
-import io.vertx.ext.web.handler.sockjs.*;
+import io.vertx.ext.web.handler.sockjs.SockJSHandler;
+import io.vertx.ext.web.handler.sockjs.BridgeOptions;
+import io.vertx.ext.bridge.PermittedOptions;
 /**
  *
  */
@@ -51,6 +48,9 @@ public class HttpApplication extends AbstractVerticle {
     private JsonObject config;
     
     private boolean online = false;
+    
+    private static final String TWITTER_EVENTBUS_NAME = "twitter-data-name";
+    private static final String TWITTER_EVENTBUS_ADDRESS = "twitter-data-address";
 
     @Override
     public void start(Future<Void> future) {
@@ -62,19 +62,14 @@ public class HttpApplication extends AbstractVerticle {
         Router router = Router.router(vertx);
         router.get("/api/greeting").handler(this::greeting);
         router.get("/api/sentiment").handler(this::sentiment);
-        router.get("/api/twit").handler(this::twit);
         router.get("/health").handler(rc -> rc.response().end("OK"));
         router.get("/*").handler(StaticHandler.create());
-        //router.get("/js/*").handler(StaticHandler.create());
         
-        //SockJSHandler sockJSHandler = SockJSHandler.create(vertx);
         BridgeOptions options = new BridgeOptions()
-            .addInboundPermitted(new PermittedOptions().setAddress("twitter-data-address"))
-            .addOutboundPermitted(new PermittedOptions().setAddress("twitter-data-address"));
+            .addInboundPermitted(new PermittedOptions().setAddress(TWITTER_EVENTBUS_ADDRESS))
+            .addOutboundPermitted(new PermittedOptions().setAddress(TWITTER_EVENTBUS_ADDRESS));
         System.out.println("BRIDGING");
         SockJSHandler sockJSHandler = SockJSHandler.create(vertx).bridge(options, event -> {
-            //if (event == null) System.out.println("FOOOOO: null");
-            //else System.out.println("FOOOOO: " + event.type());
             event.complete(true);
         });
         System.out.println("BRIDGED");
@@ -130,6 +125,8 @@ public class HttpApplication extends AbstractVerticle {
         Record record = MessageSource.createRecord(name, address);
         publish(record, completionHandler);
         
+        // We can consume the tweets from the event bus here. Nothing for us to do in this app, but
+        // useful for debugging to ensure that the event bus is receiving and publishing events
         MessageSource.<JsonObject>getConsumer(discovery, new JsonObject().put("name",name), ar -> {
             if (ar.succeeded()) {
                 MessageConsumer<JsonObject> consumer = ar.result();
@@ -164,7 +161,7 @@ public class HttpApplication extends AbstractVerticle {
     private void tweetStorm() {
         
         // Publish the services in the discovery infrastructure.
-        publishMessageSource("twitter-data-name", "twitter-data-address", rec -> {
+        publishMessageSource(TWITTER_EVENTBUS_NAME, TWITTER_EVENTBUS_ADDRESS, rec -> {
             if (!rec.succeeded()) {
                 rec.cause().printStackTrace();
             }
@@ -195,7 +192,7 @@ public class HttpApplication extends AbstractVerticle {
                                 .put("user", status.getUser().getScreenName())
                                 .put("tweet", status.getText())
                                 .put("sentiment", res.result());
-                            vertx.eventBus().publish("twitter-data-address", tweetInfo);
+                            vertx.eventBus().publish(TWITTER_EVENTBUS_ADDRESS, tweetInfo);
                         }
                     });
                 }
@@ -227,11 +224,11 @@ public class HttpApplication extends AbstractVerticle {
             }
         };
         twitterStream.addListener(listener);
-        //twitterStream.sample();
-        FilterQuery filtre = new FilterQuery();
+        //twitterStream.sample();   // this takes in the entire twitter real-time stream, unfiltered.
+        FilterQuery filter = new FilterQuery();
         String[] keywordsArray = { "infinity war" };
-        filtre.track(keywordsArray);
-        twitterStream.filter(filtre);
+        filter.track(keywordsArray);
+        twitterStream.filter(filter);
     }
 
     private void setLogLevel(String level) {
@@ -300,74 +297,12 @@ public class HttpApplication extends AbstractVerticle {
     }
     
     private void sentiment(RoutingContext rc) {
-        //double score = calculateSentimentForTweet("have a nice day");
-        //rc.response().end(score.toString());
         calculateSentimentForTweet("have a nice day", res ->{
             if (res.succeeded())
                 rc.response().end("Score: " + res.result().toString());
         });
     }
     
-    private static final String AUTH_URL = "https://api.twitter.com/oauth2/token";
-    private static final String TWEET_SEARCH_URL = "https://api.twitter.com/1.1/search/tweets.json";
-
-    private void twit(RoutingContext rc) {
-        String consumerKey = System.getenv("TWITTER_CONSUMER_KEY");
-        String consumerSecret = System.getenv("TWITTER_CONSUMER_SECRET");
-        String B64_ENCODED_AUTH = java.util.Base64.getEncoder().encodeToString((consumerKey+":"+consumerSecret).getBytes());
-        WebClient client = WebClient.create(vertx);
-
-        String queryToSearch = "vertx";
-
-        // First we need to authenticate our call.
-        String authHeader = "Basic " + B64_ENCODED_AUTH;
-        client.postAbs(AUTH_URL)
-            .as(BodyCodec.jsonObject())
-            .addQueryParam("grant_type", "client_credentials")
-            .putHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
-            .putHeader("Authorization", authHeader)
-            .send(authHandler -> {
-                // Authentication successful.
-                if (authHandler.succeeded() && 200 == authHandler.result().statusCode()) {
-                  JsonObject authJson = authHandler.result().body();
-                    String accessToken = authJson.getString("access_token");
-                    String header = "Bearer " + accessToken;
-                    // Making call to search tweets.
-                    client.getAbs(TWEET_SEARCH_URL)
-                        .as(BodyCodec.jsonObject())
-                        .addQueryParam("q", queryToSearch)
-                        .putHeader("Authorization", header)
-                        .send(handler -> {
-                            if (handler.succeeded() && 200 == handler.result().statusCode()) {
-                                rc.response().end(handler.result().body().toString());
-                            } else {
-                                rc.response().end(handler.cause().getMessage());
-                            }
-                    });
-                } else { // Authentication failed
-                    rc.response().end(authHandler.cause().getMessage());
-                }
-            });
-    }
-
-/*
-    private void twit2(RoutingContext rc) {
-        String consumerKey = System.getenv("TWITTER_CONSUMER_KEY");
-        String consumerSecret = System.getenv("TWITTER_CONSUMER_SECRET");
-        OAuth2Auth oauth = TwitterAuth.create(vertx, consumerKey, consumerSecret);
-        
-        oauth.authenticate(new JsonObject(), res -> {
-            if (res.failed()) {
-                rc.response().end("Access Token Error: " + res.cause().getMessage());
-            } else {
-                // Get the access token object (the authorization code is given from the previous step).
-                AccessToken token = (AccessToken)res.result();
-                rc.response().end(token.accessToken().toString());
-            }
-        });
-    }
-*/
-
     private Future<String> retrieveMessageTemplateFromConfiguration() {
         Future<String> future = Future.future();
         conf.getConfig(ar ->
